38ac90997d2ec0dabbde6c371dd87707c61ab517
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-21 11:46:33 +0900

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      "astro/no-exports-from-components": "error",
+      "astro/prefer-class-list-directive": "error",
+      "astro/prefer-object-class-list": "error",
+      "astro/prefer-split-class-list": "error",
+      "astro/sort-attributes": "error",
+    },
+  },
+];
