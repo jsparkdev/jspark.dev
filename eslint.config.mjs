15dada0ead8c48832b8e611b5bb96e6e2cc09326
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-22 20:39:51 +0900

    chore(eslint): update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,15 +7,33 @@ export default tseslint.config(
     ignores: ["**/dist", "**/node_modules", "**/.astro"],
   },
   eslint.configs.recommended,
-  tseslint.configs.recommended,
-  eslintPluginAstro.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
     rules: {
+      // Astro
       "astro/no-exports-from-components": "error",
       "astro/prefer-class-list-directive": "error",
       "astro/prefer-object-class-list": "error",
       "astro/prefer-split-class-list": "error",
       "astro/sort-attributes": "error",
+
+      // JavaScript
+      "func-style": ["error", "expression"],
+      "prefer-arrow-callback": "error",
+      "prefer-destructuring": "error",
+      "no-unused-vars": "error",
+      "prefer-template": "error",
+
+      // TypeScript
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: true,
+          fixStyle: "separate-type-imports",
+        },
+      ],
     },
   },
 );
