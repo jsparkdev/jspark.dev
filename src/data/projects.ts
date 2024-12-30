5c4199b730977ab6d33be07b92f5dc9b3527605d
Junseong Park <39112954+jsparkdev@users.noreply.github.com>
2024-12-30 17:20:14 +0900

    chore: update links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,15 +1,17 @@
 export type Project = {
   title: string;
-  url: string;
   description: string;
+  url: `https://${string}`;
+  github: `https://github.com/${string}`;
   technologies: string[];
 };
 
 export const projects = [
   {
     title: "jspark.dev",
+    description: "My personal website (here)",
     url: "https://jspark.dev",
-    description: "My personal website (here!)",
+    github: "https://github.com/jsparkdev/jspark.dev",
     technologies: ["Astro", "Tailwind CSS", "TypeScript"],
   },
 ] satisfies Project[];
